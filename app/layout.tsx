962e2261e7e15d44c2d3f677009ce2f4d4cc4480
Abhinay762 <your-email@example.com>
2025-07-29 18:01:13 +0530

    Added canonical url

--- app/layout.tsx
+++ app/layout.tsx
@@ -64,6 +64,8 @@ export default function RootLayout({
         <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
         <link rel="manifest" href="/site.webmanifest" />
         <link rel="shortcut icon" href="/favicon.ico" />
+        <link rel="canonical" href="https://saralcalc.online/" />
+
       </head>
       <body className={inter.className}>{children}</body>
     </html>
